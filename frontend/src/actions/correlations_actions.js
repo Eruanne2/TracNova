93d5a8d50112a2c5247110dfec63387b622812c2
Charis <bluegrassgal222@gmail.com>
2021-04-20 14:44:32 -0600

    Fix bug in VariablesReducer, complete correlations_actions

--- frontend/src/actions/correlations_actions.js
+++ frontend/src/actions/correlations_actions.js
@@ -14,3 +14,45 @@ export const receiveUserCorrelations = userCorrelations => ({
   type: RECEIVE_USER_CORRELATIONS,
   userCorrelations,
 });
+
+export const receiveCorrelationErrors = errors => ({
+  type: RECEIVE_CORRELATION_ERRORS,
+  errors,
+});
+
+export const removeCorrelation = correlationId => ({
+  type: REMOVE_CORRELATION,
+  correlationId,
+});
+
+// thunk creators
+
+export const fetchCorrelation = correlationId => dispatch => {
+  APIUtil.getCorrelationById(correlationId)
+    .then(res => dispatch(receiveCorrelation(res.data)))
+    .catch(err => dispatch(receiveCorrelationErrors(err.response.data)));
+};
+
+export const fetchUserCorrelations = userId => dispatch => {
+  APIUtil.getCorrelationsByUser(userId)
+    .then(res => dispatch(receiveUserCorrelations(res.data)))
+    .catch(err => dispatch(receiveCorrelationErrors(err.response.data)));
+};
+
+export const createCorrelation = correlationData => dispatch => {
+  APIUtil.postCorrelation(correlationData)
+    .then(res => dispatch(receiveCorrelation(res.data)))
+    .catch(err => dispatch(receiveCorrelationErrors(err.response.data)));
+};
+
+export const updateCorrelation = correlationData => dispatch => {
+  APIUtil.patchCorrelation(correlationData)
+    .then(res => dispatch(receiveCorrelation(res.data)))
+    .catch(err => dispatch(receiveCorrelationErrors(err.response.data)));
+};
+
+export const destroyCorrelation = correlationId => dispatch => {
+  APIUtil.deleteCorrelation(correlationId)
+    .then(res => dispatch(removeCorrelation(res.data._id)))
+    .catch(err => dispatch(receiveCorrelationErrors(err.response.data)));
+};
--- frontend/src/reducers/variables_reducer.js
+++ frontend/src/reducers/variables_reducer.js
@@ -14,7 +14,7 @@ const variablesReducer = (state = {}, action) => {
       });
       return newState;
     case REMOVE_VARIABLE:
-      delete newState.id;
+      delete newState[action.varId];
     default:
       return state;
   }
