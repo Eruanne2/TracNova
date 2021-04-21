7ba423b6b1e55eaf399474141b6f841f9247a296
Wenchong Lai <wenchonglai@gmail.com>
2021-04-21 16:23:31 -0700

    Add date string converters; Change functionality of log editor

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -7,18 +7,18 @@ import CorrelationsPageContainer from "./correlations/correlations_page_containe
 import LoginFormContainer from './session/login_form_container'
 import SignupFormContainer from './session/signup_form_container'
 import AppMenuContainer from "./util/app_menu_container";
-import VariablesPageContainer from "./variables/variables_page_container";
 import VariablePageContainer from "./variables/variable_page_container";
+import VariableIndexContainer from "./variables/variables_index_container"
 
 window.axios = axios;
 export default function App(){
   return (
     <>
       <ProtectedRoute path="/" component={AppMenuContainer}/>
+      <ProtectedRoute path="/" component={VariableIndexContainer}/>
       <Switch>
         <AuthRoute exact path="/login" component={LoginFormContainer}/>
         <AuthRoute exact path="/signup" component={SignupFormContainer}/>
-        <ProtectedRoute exact path="/variables" component={VariablesPageContainer}/>
         <ProtectedRoute exact path="/variables/:variableId" component={VariablePageContainer}/>
         <ProtectedRoute exact path="/correlations/:correlationId" component={CorrelationPageContainer}/>
         <ProtectedRoute exact path="/correlations" component={CorrelationsPageContainer}/>
