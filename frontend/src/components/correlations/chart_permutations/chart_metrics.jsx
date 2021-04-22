00c80612fd7278efcfb60f5a6a4e32d81a1fcb0d
Wenchong Lai <wenchonglai@gmail.com>
2021-04-21 21:23:38 -0700

    Change chart_metrics

--- frontend/src/components/correlations/chart_permutations/chart_metrics.jsx
+++ frontend/src/components/correlations/chart_permutations/chart_metrics.jsx
@@ -13,34 +13,37 @@ import {
 } from 'recharts';
 import '../../../styles/chart.css'
 
+const MOCK_DATA_1 = Array(20).fill(0).map((x, i) => ({
+  date: i,
+  value: 0.5 + Math.random() + i,
+}));
+
+const MOCK_DATA_2 = Array(20).fill(0).map((x, i) => ({
+  date: i + 3,
+  value: Math.floor(0.5 + Math.random())
+}));
+
+const MOCK_DATA_3 = Array(20).fill(0).map((x, i) => ({
+  date: i + 6,
+  value: 20 + Math.floor(0.5 + Math.random()) + Math.abs(15 - i)
+}));
+
+const mergeData = (...datasets) => {
+  const obj = {};
+
+  for (let i in datasets){
+    for (let {date, value} of Object.values(datasets[i])){
+      if (!obj[date]) obj[date] = {date}
+      obj[date][`var${i}`] = value;
+    }
+  }
+
+  return Object.values(obj);
+}
+
+const MOCK_DATA = mergeData(MOCK_DATA_1, MOCK_DATA_2, MOCK_DATA_3);
 
 export default function ChartMetrics(){
-
-  const fakeData = [];
-  for (let i = 0; i < 20; i++)
-  fakeData.push({
-    date: i, 
-    value: 0.5 + Math.random() + i,
-  })
-   
-  const fakeData2 = [];
-  for (let i = 0; i < 20; i++)
-  fakeData2.push({
-    date: i, 
-    nums: Math.floor(0.5 + (Math.random())),
-  })
-  const fakeData3 = [];
-  for (let i = 0; i < 20; i++)
-  fakeData3.push({
-    date: i, 
-    nums: 20 + Math.floor(0.5 + (Math.random())) + Math.abs(15 - (i * 2) + i),
-  })
-  const data = [];
-  for (let i = 0; i < 20; i++)
-  data.push({
-    date: i, 
-    nums: 20 + Math.floor(0.5 + (Math.random())) + Math.abs(15 - (i * 2) + i),
-  })
   // const {startIndex, endIndex} = useState;
   // const [indices, setIndices] = useState({})
   // let {startIndex, endIndex} = indices
@@ -59,9 +62,8 @@ export default function ChartMetrics(){
   // onChange={e => handleChange({startIndex: Number(e.target.value), endIndex: Number(e.target.value)})}
 
   return (
-    
     <ResponsiveContainer className="chartPrice" width={800} height={400}>
-      <LineChart data={fakeData, fakeData3}>
+      {/* <LineChart data={MOCK_DATA[0]}>
         <XAxis 
           type="number"
           dataKey="date"
@@ -80,12 +82,12 @@ export default function ChartMetrics(){
           allowDuplicatedCategories={false}
           strokeWidth="2"
           stroke='rgb(5, 200, 0)'
+          margin={{top: 20, right: 0, bottom: 20, left: 0}} 
         />
         <Tooltip backgroundColor="inherit" />
         
         <YAxis 
           yAxisId="yRight"
-          stroke='rgb(5, 0, 200)'
           type="number"
           dataKey="nums"
           axisLine={false}
@@ -93,27 +95,53 @@ export default function ChartMetrics(){
           hide={false}
           orientation='right'
           allowDuplicatedCategories={false}
+          stroke='rgb(5, 0, 200)'
           strokeWidth="2"
           margin={{top: 20, right: 0, bottom: 20, left: 0}} 
         />
         
       <Brush
-        dataKey='date'
+        dataKey="date"
         height={20} 
         stroke="#000000" 
         // startIndex={indices.startIndex}
         // endIndex={indices.endIndex}
-        onChange={brushChange}
+        // onChange={brushChange}
         syncId="sup"
       />
         
-        <Line yAxisId="yLeft" data={fakeData} classname="linear" dataKey="value" stroke="rgb(5, 200, 0)" dot={false} strokeWidth="6" />
-        
-        <Line yAxisId="yRight" data={fakeData3} classname="linear" dataKey="nums" stroke="rgb(5, 0, 220)" dot={false} strokeWidth="6" />
+        <Line yAxisId="yLeft" data={MOCK_DATA[0]} classname="linear" dataKey="value" stroke="rgb(5, 200, 0)" dot={false} strokeWidth="6" />
+        <Line yAxisId="yRight" data={MOCK_DATA[1]} classname="linear" dataKey="value" stroke="rgb(5, 0, 220)" dot={false} strokeWidth="6" />
         
         <Tooltip />
         
 
+      </LineChart> */}
+      <LineChart
+        width={500}
+        height={300}
+        data={MOCK_DATA}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <XAxis dataKey="date" />
+        <YAxis 
+          yAxisId="yLeft"
+          
+        />
+        <YAxis 
+          yAxisId="yRight"
+          
+          orientation='right'
+          
+        />
+        <Brush dataKey="date" height={30} stroke="#8884d8" />
+        <Line yAxisId="yLeft" dataKey="var0" stroke="rgb(5, 200, 0)" dot={false} strokeWidth="6"/>
+        <Line yAxisId="yRight" dataKey="var2" stroke="rgb(5, 0, 200)" dot={false} strokeWidth="6"/>
       </LineChart>
     </ResponsiveContainer>
   )
