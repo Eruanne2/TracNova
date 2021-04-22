5df6b906cd69a805c2c8d66217a44bafb3cd904b
Chris Hanma <hongchris96@hotmail.com>
2021-04-22 16:27:43 -0700

    Fix 404 css add link

--- frontend/src/components/static_pages/page_not_found.jsx
+++ frontend/src/components/static_pages/page_not_found.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import doge from '../../images/doge404.png'
 import '../../styles/page404.css';
+import {Link} from 'react-router-dom';
 
 export default function Splash() {
   return(
     <div className="page-404">
       <h1>404</h1>
       <p>Wow! Such Lost in Space ~~</p>
+      <Link to="/login" className="go-back">Please take me home!</Link>
       <img src={doge} alt="doge"/>
     </div>
   )
