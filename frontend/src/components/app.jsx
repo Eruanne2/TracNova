4743786fe2e7f2721492f6347e35ab755eadb1b8
Charis <bluegrassgal222@gmail.com>
2021-04-21 23:39:13 -0600

    Make skeletons and routes for splash and 404 pages

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -10,6 +10,8 @@ import AppMenuContainer from "./util/app_menu_container";
 import DashboardContainer from './dashboard/dashboard_container';
 import VariablePageContainer from "./variables/variable_page_container";
 import VariableIndexContainer from "./variables/variables_index_container"
+import PageNotFound from './static_pages/page_not_found';
+import Splash from './static_pages/splash';
 
 window.axios = axios;
 export default function App(){
@@ -17,6 +19,7 @@ export default function App(){
     <div>
       <ProtectedRoute path="/" component={AppMenuContainer}/>
       <Switch>
+        <AuthRoute exact path='/' component={Splash}/>
         <AuthRoute exact path="/login" component={LoginFormContainer}/>
         <AuthRoute exact path="/signup" component={SignupFormContainer}/>
         <ProtectedRoute exact path="/variables/:variableId" component={VariablePageContainer}/>
@@ -26,6 +29,7 @@ export default function App(){
         <Route exact path="/">
           <Redirect to="/dashboard"/>
         </Route>  
+        <Route path='*' component={PageNotFound}/>
         <Route >
           
         </Route>
