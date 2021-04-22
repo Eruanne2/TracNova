7a5b25331067bb6de882fda6e687a200c7b9d06e
Charis <bluegrassgal222@gmail.com>
2021-04-22 13:10:54 -0600

    Flesh out AddEntryForm, still not functional

--- frontend/src/components/util/add_entry_form.jsx
+++ frontend/src/components/util/add_entry_form.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function AddEntryForm({defaultVar}) {
+export default function AddEntryForm({updateVariable, variables, defaultVar}) {
+  const [_variable, _setVariable] = useState(defaultVar || undefined);
+  
+  useEffect(() => {
+    if (!!defaultVar) _setVariable(defaultVar)
+  }, [defaultVar]);
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    updateVariable(_variable)
+  };
+
+  const submitYesterday = e => {
+    e.preventDefault();
+    let newVar = Object.assign({}, _variable);
+    newVar[getToday()] = _variable[getYesterday()];
+    updateVariable(newVar);
+  };
+
+  const getYesterday = () => {
+    let date = new Date();
+    let dateString = ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + (date.getDate() - 1)).slice(-2) + '/' + date.getFullYear();
+    return dateString;
+  };
+
+  const getToday = () => {
+    let date = new Date();
+    let dateString = ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2) + '/' + date.getFullYear();
+    return dateString;
+  }
+
+  const getCustomInput = variable => {
+    if (!variable) return null;
+    switch(variable.unit){
+      case 'boolean':
+        return <p>yes/no input</p>
+      case 'rating':
+        return <p>rating input</p>
+      default:
+        return <p>metric inupt</p>
+    }
+  };
+
+  debugger
   return(
     <div className='entry-form-div'>
-      {defaultVar ? <h1>Update {defaultVar.name}</h1> : <h1>Log a Habit</h1>}
+      {!!defaultVar ? <h1>Update {defaultVar.name}</h1> : <h1>Log a Habit</h1>}
+      <form>
+        {!defaultVar &&
+          <select value={_variable}>
+            <option>Select Habit</option>
+            {variables && Object.values(variables).map((variable, idx) => (
+              <option value={variable} key={idx}>{variable.name}</option>
+            ))}
+          </select>
+        }
+        {(!!_variable && !!_variable.dailylogs[getYesterday()]) && <button onClick={submitYesterday}>Repeat yesterday's Entry</button>}
+        {getCustomInput(_variable)}
+        <input type='submit' onClick={handleSubmit} value='Add Entry' />
+      </form>
     </div>
   )
 };
---
+++ frontend/src/components/util/add_entry_form_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { updateVariable } from '../../actions/variables_actions';
+import AddEntryForm from './add_entry_form';
+
+const mapSTP = state => ({
+  variables: Object.values(state.entities.variables)
+});
+const mapDTP = dispatch => ({
+  updateVariable: entryData => dispatch(updateVariable(entryData))
+});
+
+export default connect(null, mapDTP)(AddEntryForm);
