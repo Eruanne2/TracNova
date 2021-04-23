76eb663b2aa1c56f9451537e2ef8ee3db5576728
Charis <bluegrassgal222@gmail.com>
2021-04-23 12:20:03 -0600

    Remove variables_page and variables_page_container

--- frontend/src/components/variables/variables_page_container.jsx
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import VariablesPage from "./variables_page";
-
-const mapSTP = ({entities}) => ({
-  variables: Object.values(entities.variables)
-});
-
-export default connect(mapSTP)(VariablesPage);
