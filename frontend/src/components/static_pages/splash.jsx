6715c3ce06eb77a92d74b491b62b97765834f18b
Chris Hanma <hongchris96@hotmail.com>
2021-04-22 18:10:06 -0700

    update splash css

--- frontend/src/components/static_pages/splash.jsx
+++ frontend/src/components/static_pages/splash.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import '../../styles/splash.css'
 // import strange_space from '../../images/strange_space.jpeg';
-import logo from '../../images/logo-black.png'
+import logo from '../../images/logo-white.png'
 import {Link} from 'react-router-dom';
 
 export default function Splash() {
   return(
-    <div className="splash-div">
-      <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/stars.png'})` }} className="stars"/>
-      <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/twinkling.png'})` }} className="twinkling"/>
-      <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/clouds.png'})` }} className="clouds"/>
-      
+    <div className="splash-div"> 
       <img src={logo}/>
       <Link to="/login" className="go-back">LOG IN HERE</Link>
     </div>
