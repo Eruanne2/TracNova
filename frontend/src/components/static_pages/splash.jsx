212a1b0b144520c5d4d1b8cfb02a94ecb3849207
Justin Cheasty <cheasty@gmail.com>
2021-04-23 14:52:00 -0700

    animate front splash fix login button

--- frontend/src/components/static_pages/splash.jsx
+++ frontend/src/components/static_pages/splash.jsx
@@ -8,13 +8,12 @@ import {Link} from 'react-router-dom';
 export default function Splash() {
   return(
     <div className="splash-div"> 
-        {/* <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/strange_space.jpeg'})` }} className="strange"/> */}
         <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/stars.png'})` }} className="stars"/>
         <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/twinkling.png'})` }} className="twinkling"/>
         <div style={{ backgroundImage: `(${process.env.PUBLIC_URL + 'images/clouds.png'})` }} className="clouds"/>
       <div className="splash-inner-div">
-        <img src={logo}/>
-        <Link to="/login" className="go-back">LOG IN HERE</Link>
+        <img className="logo" src={logo}/>
+        <Link className="logo" to="/login" className="go-back">LOG IN HERE</Link>
       </div>
     </div>
   )
