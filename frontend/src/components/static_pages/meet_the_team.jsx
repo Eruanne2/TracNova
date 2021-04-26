62a8380919fc27e3f55f0ad870a8cecbd9f6e076
Charis <bluegrassgal222@gmail.com>
2021-04-25 23:47:57 -0600

    Complete 'Meet the Team' page

--- frontend/src/components/static_pages/meet_the_team.jsx
+++ frontend/src/components/static_pages/meet_the_team.jsx
@@ -5,6 +5,7 @@ import charisGinn from '../../images/charis-ginn.jpg';
 import chrisHong from '../../images/chris-hong.jpg';
 import wenchongLai from '../../images/wenchong-lai.jpg';
 import justinCheasty from '../../images/justin-cheasty.jpeg';
+import '../../styles/meet_the_team.css';
 
 export default function MeetTheTeam() {
   return(
@@ -12,35 +13,43 @@ export default function MeetTheTeam() {
       <h1>The TracNova Team</h1>
       <ul>
         <li>
-          <img src={charisGinn} className="member-photo" alt="Charis Ginn, Team Lead" height="150"/>
+          <img src={charisGinn} className="member-photo" alt="Charis Ginn, Team Lead" height="200"/>
           <h2>Charis Ginn</h2>
           <h3>Team Lead</h3>
-          <a href="https://github.com/Eruanne2"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
-          <a href="www.linkedin.com/in/charis-ginn-9abb93173"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          <div>
+            <a href="https://github.com/Eruanne2"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
+            <a href="www.linkedin.com/in/charis-ginn-9abb93173"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          </div>
         </li>
 
         <li>
-          <img src={chrisHong} className="member-photo" alt="Chris Hong, Backend Lead" height="150"/>
+          <img src={chrisHong} className="member-photo" alt="Chris Hong, Backend Lead" height="200"/>
           <h2>Chris Hong</h2>
           <h3>Backend Lead</h3>
-          <a href="https://github.com/hongchris96"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
-          <a href="https://www.linkedin.com/in/chen-wei-christopher-hong-4b189162/"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          <div>
+            <a href="https://github.com/hongchris96"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
+            <a href="https://www.linkedin.com/in/chen-wei-christopher-hong-4b189162/"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          </div>
         </li>
 
         <li>
-          <img src={wenchongLai} className="member-photo" alt="Wenchong Lai, Frontend Lead" height="150"/>
+          <img src={wenchongLai} className="member-photo" alt="Wenchong Lai, Frontend Lead" height="200"/>
           <h2>Wenchong Lai</h2>
           <h3>Frontend Lead</h3>
-          <a href="https://github.com/wenchonglai"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
-          <a href="https://www.linkedin.com/in/wenchong-lai-4296424b/"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          <div>
+            <a href="https://github.com/wenchonglai"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
+            <a href="https://www.linkedin.com/in/wenchong-lai-4296424b/"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          </div>
         </li>
 
         <li>
-          <img src={justinCheasty} className="member-photo" alt="Justin Cheasty, Graphics Lead" height="150"/>
+          <img src={justinCheasty} className="member-photo" alt="Justin Cheasty, Graphics Lead" height="200"/>
           <h2>Justin Cheasty</h2>
           <h3>Graphics Lead</h3>
-          <a href="https://github.com/justinnnnnnnn"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
-          <a href="https://www.linkedin.com/in/justin-cheasty-2a521a14/"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          <div>
+            <a href="https://github.com/justinnnnnnnn"><img src={githubLogo} className="social-logo" alt="Github Logo" height="60"/></a>
+            <a href="https://www.linkedin.com/in/justin-cheasty-2a521a14/"><img src={linkedinLogo} className="social-logo" alt="LinkedIn Logo" height="61"/></a>
+          </div>
         </li>
       </ul>
     </div>
